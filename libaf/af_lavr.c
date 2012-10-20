c15204007d217b681ef021d8586c02e43ffd6115
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-10-20 17:01:15 +0200

    lavr

--- libaf/af_lavr.c
+++ libaf/af_lavr.c
@@ -16,19 +16,19 @@ static int control(struct af_instance_s *af, int cmd, void *arg)
         af->data->rate,af->data->nch,af_fmt2str_short(af->data->format));
             return AF_OK;
     }
-    mp_msg(MSGT_AFILTER, MSGL_ERR, "[lavr] control ok.\n");
+    mp_msg(MSGT_AFILTER, MSGL_INFO, "[lavr] control ok.\n");
     return AF_UNKNOWN;
 }
 
 static void uninit(struct af_instance_s *af)
 {
-    mp_msg(MSGT_AFILTER, MSGL_ERR, "[lavr] uinit ok.\n");
+    mp_msg(MSGT_AFILTER, MSGL_INFO, "[lavr] uinit ok.\n");
     talloc_free(af->data);
 }
 
 static af_data_t* play(struct af_instance_s *af, struct af_data_s *data)
 {
-    mp_msg(MSGT_AFILTER, MSGL_ERR, "[lavr] play ok.\n");
+    mp_msg(MSGT_AFILTER, MSGL_INFO, "[lavr] play ok.\n");
     return data;
 }
 
@@ -40,16 +40,25 @@ static int af_open(struct af_instance_s *af){
     af->data     = talloc_ptrtype(NULL, af->data);
 
     if (af->data == NULL) {
-        mp_msg(MSGT_AFILTER, MSGL_ERR, "[lavr] init ok (loal).\n");
+        mp_msg(MSGT_AFILTER, MSGL_ERR, "[lavr] initialization failed.\n");
         return AF_ERROR;
     }
 
-    mp_msg(MSGT_AFILTER, MSGL_ERR, "[lavr] init ok.\n");
+    mp_msg(MSGT_AFILTER, MSGL_INFO, "[lavr] init OK.\n");
     return AF_OK;
 }
 
+af_info_t af_info_lavr_interleave = {
+    "Audio samples interleaving filters based on libavresample",
+    "lavr_interleave",
+    "Stefano Pigozzi",
+    "",
+    AF_FLAGS_REENTRANT,
+    af_open
+};
+
 af_info_t af_info_lavr = {
-    "Audio resampling filter based on libavresample",
+    "Audio resampling filters based on libavresample",
     "lavr",
     "Stefano Pigozzi",
     "",
