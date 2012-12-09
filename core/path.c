46c0479c22e38420cf96dc3015341ab74125ba1c
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-12-09 15:05:21 +0100

    path: add mp_find_config_file and reorganize some of the code
    Add `mp_find_config_file` to search different known paths and use that in
    ass_mp to look for the fontconfig configuration file.
    
    Some incidental changes spawned by this feature where:
    
     * Buffer allocation for the strings containing the paths is now performed
       with talloc. All of the allocations are done on a NULL context, but it still
       improves readability of the code.
     * Move the OSX function for lookup inside of a bundle: this code path was
       currently not used by the bundle generated with `make osxbundle`. The plan
       is to use it again in a future commit to get a fontconfig config file.

--- core/path.c
+++ core/path.c
@@ -23,6 +23,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,25 +34,54 @@
 #include "config.h"
 #include "core/mp_msg.h"
 #include "core/path.h"
+#include "talloc.h"
+#include "osdep/io.h"
 
-#ifdef CONFIG_MACOSX_BUNDLE
-#include <CoreFoundation/CoreFoundation.h>
-#include <unistd.h>
-#elif defined(__MINGW32__)
+#if defined(__MINGW32__)
 #include <windows.h>
 #elif defined(__CYGWIN__)
 #include <windows.h>
 #include <sys/cygwin.h>
 #endif
 
-#include "talloc.h"
+#ifdef CONFIG_MACOSX_BUNDLE
+#include "osdep/macosx_bundle.h"
+#endif
 
-#include "osdep/io.h"
-
-char *get_path(const char *filename)
+static char *get_global_path(const char *filename)
 {
-    char *homedir;
-    char *buff;
+    assert(filename);
+    return talloc_asprintf(NULL, "%s/%s", MPLAYER_CONFDIR, filename);
+}
+
+typedef char *(*lookup_fun)(const char *);
+static const lookup_fun config_lookup_functions[] = {
+    mp_get_path,
+#ifdef CONFIG_MACOSX_BUNDLE
+    get_bundled_path,
+#endif
+    get_global_path,
+    NULL
+};
+
+char *mp_find_config_file(const char *filename)
+{
+    for (int i = 0; config_lookup_functions[i] != NULL; i++) {
+        char *path = config_lookup_functions[i](filename);
+        if (!path) continue;
+
+        if (mp_path_exists(path)) {
+            return path;
+        } else {
+            talloc_free(path);
+        }
+    }
+    return NULL;
+}
+
+char *mp_get_path(const char *filename)
+{
+    char *homedir, *buff = NULL;
 #ifdef __MINGW32__
     static char *config_dir = "/mpv";
 #else
@@ -60,24 +90,13 @@ char *get_path(const char *filename)
 #if defined(__MINGW32__) || defined(__CYGWIN__)
     char exedir[260];
 #endif
-    int len;
-#ifdef CONFIG_MACOSX_BUNDLE
-    struct stat dummy;
-    CFIndex maxlen = 256;
-    CFURLRef res_url_ref = NULL;
-    CFURLRef bdl_url_ref = NULL;
-    char *res_url_path = NULL;
-    char *bdl_url_path = NULL;
-#endif
-
-    if ((homedir = getenv("MPV_HOME")) != NULL)
+    if ((homedir = getenv("MPV_HOME")) != NULL) {
         config_dir = "";
-    else if ((homedir = getenv("HOME")) == NULL)
+    } else if ((homedir = getenv("HOME")) == NULL) {
 #if defined(__MINGW32__) || defined(__CYGWIN__)
     /* Hack to get fonts etc. loaded outside of Cygwin environment. */
-    {
         int i, imax = 0;
-        len = (int)GetModuleFileNameA(NULL, exedir, 260);
+        int len = (int)GetModuleFileNameA(NULL, exedir, 260);
         for (i = 0; i < len; i++)
             if (exedir[i] == '\\') {
                 exedir[i] = '/';
@@ -85,63 +104,17 @@ char *get_path(const char *filename)
             }
         exedir[imax] = '\0';
         homedir = exedir;
-    }
 #else
         return NULL;
 #endif
-    len = strlen(homedir) + strlen(config_dir) + 1;
-    if (filename == NULL) {
-        if ((buff = malloc(len)) == NULL)
-            return NULL;
-        sprintf(buff, "%s%s", homedir, config_dir);
+    }
+
+    if (filename) {
+        buff = talloc_asprintf(NULL, "%s%s/%s", homedir, config_dir, filename);
     } else {
-        len += strlen(filename) + 1;
-        if ((buff = malloc(len)) == NULL)
-            return NULL;
-        sprintf(buff, "%s%s/%s", homedir, config_dir, filename);
+        buff = talloc_asprintf(NULL, "%s%s", homedir, config_dir);
     }
 
-#ifdef CONFIG_MACOSX_BUNDLE
-    if (stat(buff, &dummy)) {
-
-        res_url_ref = CFBundleCopyResourcesDirectoryURL(CFBundleGetMainBundle());
-        bdl_url_ref = CFBundleCopyBundleURL(CFBundleGetMainBundle());
-
-        if (res_url_ref && bdl_url_ref) {
-
-            res_url_path = malloc(maxlen);
-            bdl_url_path = malloc(maxlen);
-
-            while (!CFURLGetFileSystemRepresentation(res_url_ref, true,
-                                                     res_url_path, maxlen)) {
-                maxlen *= 2;
-                res_url_path = realloc(res_url_path, maxlen);
-            }
-            CFRelease(res_url_ref);
-
-            while (!CFURLGetFileSystemRepresentation(bdl_url_ref, true,
-                                                     bdl_url_path, maxlen)) {
-                maxlen *= 2;
-                bdl_url_path = realloc(bdl_url_path, maxlen);
-            }
-            CFRelease(bdl_url_ref);
-
-            if (strcmp(res_url_path, bdl_url_path) == 0)
-                res_url_path = NULL;
-        }
-
-        if (res_url_path && filename) {
-            if ((strlen(filename) + strlen(res_url_path) + 2) > maxlen)
-                maxlen = strlen(filename) + strlen(res_url_path) + 2;
-            free(buff);
-            buff = malloc(maxlen);
-            strcpy(buff, res_url_path);
-
-            strcat(buff, "/");
-            strcat(buff, filename);
-        }
-    }
-#endif
     mp_msg(MSGT_GLOBAL, MSGL_V, "get_path('%s') -> '%s'\n", filename, buff);
     return buff;
 }
